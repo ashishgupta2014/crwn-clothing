ac451ceded957c8f5e523bc6c16f72fe2e8da31b
ashishgupta2014 <ashish.2007g@gmail.com>
2020-06-08 17:28:42 +0530

    firestore and reduce HOC

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,12 +1,17 @@
-import SHOP_DATA from './shop.data';
 
+import ShopActionTypes from './shop.types';
 
 const INITIAL_STAE = {
-    collections: SHOP_DATA
+    collections: null
 };
 
 const shopReducer = (state = INITIAL_STAE, action) => {
     switch(action.type) {
+        case ShopActionTypes.UPDATE_COLLECTIONS:
+            return {
+                ...state,
+                collections: action.payload
+            }
         default:
             return state
     }
