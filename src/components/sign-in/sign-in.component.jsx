036b2b1a972ff6a620fc74eab852d2ef40616c0a
ashishgupta2014 <ashish.2007g@gmail.com>
2020-06-21 21:49:26 +0530

    added saga

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+import { googleSignInStart, emailSignInStart } from '../../redux/user/user.actions';
+
 
 import './sign-in.styles.scss';
 
@@ -19,14 +21,9 @@ class SignIn extends React.Component {
 
     handleSubmit = async event => {
         event.preventDefault();
+        const { emailSignInStart } = this.props;
         const { email, password } = this.state;
-        try {
-            await auth.signInWithEmailAndPassword(email, password);
-            this.setState({ email: '', password: ''});
-        } catch(error){
-            console.log(error);
-        }
-        
+        emailSignInStart(email, password);
     }
 
     handleChange = event => {
@@ -36,6 +33,7 @@ class SignIn extends React.Component {
 
 
     render() {
+        const { googleSignInStart } = this.props;
         return(
             <div className='sign-in'>
                 <h2>I have already an account</h2>
@@ -59,7 +57,7 @@ class SignIn extends React.Component {
                     <div className="buttons">
                         <CustomButton type="submit"> SignIn </CustomButton>
 
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                        <CustomButton type="button" onClick={googleSignInStart} isGoogleSignIn>
                             {' '}
                             SignIn with Google {' '}
                         </CustomButton>
@@ -71,4 +69,9 @@ class SignIn extends React.Component {
 }
 
 
-export default SignIn;
+const mapDispatchToProps = dispatch => ({
+    googleSignInStart: () => dispatch(googleSignInStart()),
+    emailSignInStart: (email, password) => dispatch(emailSignInStart({ email, password }))
+})
+
+export default connect(null, mapDispatchToProps)(SignIn);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -70,6 +70,16 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   };
 
 
+  export const getCurrentUser = () => {
+    return new Promise((resolve, reject) => {
+      const unsubscribe = auth.onAuthStateChanged(userAuth => {
+        unsubscribe();
+        resolve(userAuth);
+      }, reject);
+    });
+  };
+  
+
 
 
 firebase.initializeApp(config);
@@ -81,10 +91,12 @@ firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-const provider =new firebase.auth.GoogleAuthProvider();
 
-provider.setCustomParameters({ prompt: 'select_account' });
 
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const googleProvider =new firebase.auth.GoogleAuthProvider();
+
+googleProvider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
 
 export default firebase;
